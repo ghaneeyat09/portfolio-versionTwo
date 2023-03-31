c4c0a448ed0f3ea8f1cf237bcf03560d5a8b639e
ghaneeyat09 <69092604+ghaneeyat09@users.noreply.github.com>
2023-03-31 15:05:06 +0100

    new commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
-import * as React from "react"
+import * as React from "react";
+import * as styles from "../styles/home.module.css";
+import "../styles/global.css";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <>
+             <div className={styles.background}>
+                <div className={styles.logo}>
+                  <h3>G&bull;&bull;</h3>
+                </div>
+                <div className={styles.container}>
+                  <h1>I build worldclass and innovative <span className={styles.text}>websites</span> using <span className={styles.text}> modern</span> technologies</h1>
+                </div>
+             </div>
+         </>
 }
